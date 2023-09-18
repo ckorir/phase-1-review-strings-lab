3bbc6ccf6e6aad76eed3a5a8c9427338c7f9e252
ckorir <chrisruto.k@gmail.com>
2023-09-18 10:24:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Chris Ruto';
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = `${welcomeMessage}`.toUpperCase();
+let shortGreeting = `Welcome, ` + currentUser.charAt(0) + `!`;
